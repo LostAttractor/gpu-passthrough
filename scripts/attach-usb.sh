36db7b8b22e825aad5762a63bd863966d793c323
ChaosAttractor <lostattractor@gmail.com>
2023-09-04 11:08:55 +0800

    Create usb devices configuration

---
+++ scripts/attach-usb.sh
@@ -0,0 +1,3 @@
+virt-xml win11 --update --add-device  --hostdev 046d:c539,type=usb	# Mouse
+virt-xml win11 --update --add-device  --hostdev 05ac:024f,type=usb 	# Keyboard
+virt-xml win11 --update --add-device  --hostdev 0a73:002b,type=usb	# Mic
